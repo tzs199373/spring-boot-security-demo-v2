f2c173bf61bed9f0883f091950d5f411f38a5058
����ɽ <544610616@qq.com>
2020-10-24 22:36:50 +0800

    1

--- src/main/java/com/security/SpringbootSecurityApplication.java
+++ src/main/java/com/security/SpringbootSecurityApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.security.mapperr")//使用MapperScan批量扫描所有的Mapper接口；
+@MapperScan("com.security.mapper")//使用MapperScan批量扫描所有的Mapper接口；
 public class SpringbootSecurityApplication {
 
 	public static void main(String[] args) {
